1645f3bdd879552fee5268d5174bdeb0ad03ce01
Đào Minh Tùng <daotungsl@gmail.com>
2025-06-25 21:03:17 +0700

    up

--- src/data/seedData.cpp
+++ src/data/seedData.cpp
@@ -9,10 +9,8 @@ void insertSeedData() {
     std::string phone1 = "099";
     std::string password1 = PasswordUtils::hashPassword("123");
     Wallet wallet1;
-    User user1(username1, password1, UserRole::User, username1, wallet1.getWalletId(), phone1);
+    User user1(username1, password1, UserRole::Manager, username1, wallet1.getWalletId(), phone1);
     wallet1.setPoints(1000);
-
-
 
     std::string username2 = "daotung1";
     std::string phone2 = "097";
@@ -32,8 +30,10 @@ void insertSeedData() {
     bool ok2 = UserFileHelper::saveNewWallet(wallet1);
     bool ok3 = UserFileHelper::saveNewUser(user2);
     bool ok4 = UserFileHelper::saveNewWallet(wallet2);
+    bool ok5 = UserFileHelper::saveNewUser(user3);
+    bool ok6 = UserFileHelper::saveNewWallet(wallet3);
 
-    if (ok1 && ok2 && ok3 && ok4) {
+    if (ok1 && ok2 && ok3 && ok4 && ok5 && ok6) {
         std::cout << "\n\n==> Seed data thanh cong!\n";
     } else {
         std::cerr << "\n\n==> Seed data that bai.\n";
